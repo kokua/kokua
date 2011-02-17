056a3d65033cdec18d4315b05a1d1a69e52358b5
Armin Weatherwax <Armin.Weatherwax@gmail.com>
2011-02-17 11:57:53 +0100

    Linux64: Split Qt and llqtwebkit in 2 packages, add the new llqtwebkit to install.xml
    HACK: *ORDER OF DOWNLOAD MATTERS* lindenqt currently is the old llqtwebkit package, which contains
    Qt 4.6 libraries and the llqtwebkit library. The new llqtwebkit library overrides the one of the lindenqt
    package. Will be unhacked with the next update of Qt.

--- indra/cmake/WebKitLibPlugin.cmake
+++ indra/cmake/WebKitLibPlugin.cmake
@@ -6,6 +6,14 @@ if (STANDALONE)
   set(WEBKITLIBPLUGIN OFF CACHE BOOL
       "WEBKITLIBPLUGIN support for the llplugin/llmedia test apps.")
 else (STANDALONE)
+  if(LINUX AND ${ARCH} STREQUAL "x86_64")
+     use_prebuilt_binary(lindenqt) 		# HACK: *ORDER OF DOWNLOAD MATTERS*
+						# lindenqt currently also contains a deprecated version
+						# of llqtwebkit, that is overridden by the one downloaded by
+						# "use_prebuilt_binary(llqtwebkit)", which is actually used.
+						# TODO: unhack with the next update of Qt
+  endif(LINUX AND ${ARCH} STREQUAL "x86_64")
+
   use_prebuilt_binary(llqtwebkit)
   set(WEBKITLIBPLUGIN ON CACHE BOOL
       "WEBKITLIBPLUGIN support for the llplugin/llmedia test apps.")
