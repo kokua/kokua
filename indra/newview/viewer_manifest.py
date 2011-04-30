2fdf2921717d0d26aae26d161e67587297b1de0e
Armin Weatherwax <Armin.Weatherwax@gmail.com>
2011-04-30 17:43:20 +0200

    a few more pokes on the linux scripts
    * actually register hop
    * make "Kokua" the desktop entry instead of "kokua-viewer"
    * have the icon

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -809,7 +809,7 @@ class LinuxManifest(ViewerManifest):
     def construct(self):
         super(LinuxManifest, self).construct()
         self.path("licenses-linux.txt","licenses.txt")
-        self.path("res/kokua_icon.png")
+        self.path("res/kokua_icon.png", "kokua_icon.png")
         if self.prefix("linux_tools", dst=""):
             self.path("client-readme.txt","README-linux.txt")
             self.path("client-readme-voice.txt","README-linux-voice.txt")
