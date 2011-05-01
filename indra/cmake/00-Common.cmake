b1e67ee0287a982e7782d9eca5a7de82fbc18307
Armin Weatherwax <Armin.Weatherwax@gmail.com>
2011-05-01 20:22:27 +0200

    And comment linux64 compiler flags out again, it tries to stage them >_>

--- indra/cmake/00-Common.cmake
+++ indra/cmake/00-Common.cmake
@@ -186,7 +186,7 @@ if (LINUX)
   if (${ARCH} STREQUAL "x86_64")
      add_definitions(-DLINUX64=1 -pipe)
      #linux64 atm crashes usin -O2
-     set(CMAKE_CXX_FLAGS_RELEASE "-fomit-frame-pointer -mmmx -msse -mfpmath=sse msse2 -ffast-math - ftree-vectorize -fweb -fexpensive-optimizations -frename-registers ${CMAKE_CXX_FLAGS_RELEASE}")
+#      set(CMAKE_CXX_FLAGS_RELEASE "-fomit-frame-pointer -mmmx -msse -mfpmath=sse msse2 -ffast-math - ftree-vectorize -fweb -fexpensive-optimizations -frename-registers ${CMAKE_CXX_FLAGS_RELEASE}")
   else(${ARCH} STREQUAL "x86_64")
       set(CMAKE_CXX_FLAGS_RELEASE "-O2 ${CMAKE_CXX_FLAGS_RELEASE}")
   endif (${ARCH} STREQUAL "x86_64")
